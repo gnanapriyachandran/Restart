d08c002fd113b306e11aaed63cc3199d7d341e8d
Gnanapriya <gnanapriya.cr@gmail.com>
2022-12-21 19:49:08 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by gnanapriya.chandran on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
